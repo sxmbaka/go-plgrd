6afd3a21a276038f9458473d6b8c76185c4906f6
Yatharth Singh <yatharth.singhhh@gmail.com>
2024-04-25 11:07:13 +0530

    Added naming conventions

--- readme.md
+++ readme.md
@@ -7,7 +7,9 @@
 (You might need to study [Operators](./operators/) in case of any anomalies)
 3. [Type Conversions](./variables/type-conversions.go)
 4. [Type Inference](./variables/type-inference.go)
-4. [Type Inference](./variables/type-inference.go) (read further at https://go.dev/blog/type-inference)
+4. [Type Inference](./variables/type-inference.go)<br>
+(Read further at https://go.dev/blog/type-inference)
+6. [Variable Naming Conventions](./variables/variable-naming-conventions.go)
 
 
 __Tip__: _Create a `playground` directory in the `root` directory and then test these examples yourself._
---
+++ variables/variable-naming-conventions.go
@@ -0,0 +1,12 @@
+package variables
+
+/*
+Below are naming conventions for variables and constant in golang
+
+1. A variable or constant name can only start with a letter or an underscore.
+2. It can be followed by any number of letters, numbers or underscores after that
+3. Go is case sensitive so uppercase and lowercase letters are treated differently.
+4. The variable or constant name cannot be any keyword name in Go
+5. There is no limit on the length of variable or constant name. But is advisable to have the variable name of optimum length.
+6. Finally, the convention in Go is to use MixedCaps or mixedCaps rather than underscores to write multiword names.
+*/
