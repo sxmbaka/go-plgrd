896071bb8cd102a8f93a8d484ae2ec569cf06da8
Yatharth Singh <yatharth.singhhh@gmail.com>
2024-04-25 14:07:30 +0530

    Refactor main function to handle command-line arguments in main.go

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"os"
+
+	"github.com/sxmbaka/go-plgrd/playground"
 	"github.com/sxmbaka/go-plgrd/variables"
 )
 
 func main() {
-	variables.TestAllVariables()
-
-	variables.TestAllOperations()
-
-	variables.TypeConversion()
-
-	variables.TypeInference()
+	// Get command-line arguments excluding the program name
+	args := os.Args[1:]
+	if len(args) > 0 {
+		switch args[0] {
+		case "variables":
+			variables.TestAllVariables()
+		case "operations":
+			variables.TestAllOperations()
+		case "conversion":
+			variables.TypeConversion()
+		case "inference":
+			variables.TypeInference()
+		case "playground":
+			playground.Test()
+		default:
+			// Handle invalid command-line argument
+			println("Invalid argument")
+		}
+	} else {
+		// Handle no command-line argument to run all
+		variables.TestAllOperations()
+		variables.TestAllVariables()
+		variables.TypeConversion()
+		variables.TypeInference()
+	}
 }
